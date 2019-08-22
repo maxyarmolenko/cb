79d6f23920bb5791e55d53e7a26d46bce3ac09ed
Max <maksym_yarmolenko@hotmail.com>
2019-08-22 18:47:44 +0300

    PiandENumbersApp is changed

--- Starter/L2/PiandENumbersApp/PiandENumbersApp/Program.cs
+++ Starter/L2/PiandENumbersApp/PiandENumbersApp/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace PiandENumbersApp
+﻿using System;
+
+namespace PiandENumbersApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int Main(string[] args)
         {
+            double pi = 3.141592653d;
+            Console.WriteLine(pi);
+            Console.ReadKey();
+
+            double e = 2.7182818284590452d;
+            Console.WriteLine(e);
+
+            Console.ReadKey();
         }
     }
 }

f1a0cc5a03497fc43233bac3047cb0cb5ba6ddd9
Max <maksym_yarmolenko@hotmail.com>
2019-08-22 20:54:24 +0300

    IntVariablesApp is not finished

---
+++ Starter/L3/IntVariablesApp/IntVariablesApp/IntVariablesApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntVariablesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 12, z = 3;
+            x += y - x++ * z;
+            Console.WriteLine(x);
+            z = --x – y * 5; }
+
+        Console.WriteLine();
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}

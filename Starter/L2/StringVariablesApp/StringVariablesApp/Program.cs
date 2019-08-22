6b1c2b8401d77ec8c3eb3cfa3e4aca41938c9aaa
Max <maksym_yarmolenko@hotmail.com>
2019-08-22 20:21:31 +0300

    StringVariablesApp is done

---
+++ Starter/L2/StringVariablesApp/StringVariablesApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringVariablesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "\nмоя строка 1";
+            Console.WriteLine(a);
+            string b = "\tмоя строка 2";
+            Console.WriteLine(b);
+            string c = "\aмоя строка 3";
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}

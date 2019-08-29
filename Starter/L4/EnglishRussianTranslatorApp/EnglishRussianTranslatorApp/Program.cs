4bdc1959b6e06eaf4785c7e15d277a70463a108b
Max <maksym_yarmolenko@hotmail.com>
2019-08-29 22:04:14 +0300

    EnglishRussianTranslatorApp is fixed

--- Starter/L4/EnglishRussianTranslatorApp/EnglishRussianTranslatorApp/Program.cs
+++ Starter/L4/EnglishRussianTranslatorApp/EnglishRussianTranslatorApp/Program.cs
@@ -5,71 +5,77 @@ namespace EnglishRussianTranslatorApp
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Вас приветствует русско-английский переводчик!!!");
-            Console.WriteLine("Введите пожалуйста слово на русском языке");
-
-            string word = Console.ReadLine();
-            switch (word)
+            string word;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.WriteLine("Вас приветствует русско-английский переводчик!!!");
+            do
             {
-                case "солнечный":
+                Console.ForegroundColor = ConsoleColor.White;
+                System.Console.WriteLine("Введите пожалуйста слово на русском языке");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                word = System.Console.ReadLine();
+                switch (word)
                 {
-                    Console.WriteLine("sunny");
-                    break;
+                    case "солнечный":
+                    {
+                        Console.WriteLine("sunny");
+                        break;
+                    }
+                    case "дождь":
+                    {
+                        Console.WriteLine("rain");
+                        break;
+                    }
+                    case "холод":
+                    {
+                        Console.WriteLine("cold");
+                        break;
+                    }
+                    case "ветер":
+                    {
+                        Console.WriteLine("wind");
+                        break;
+                    }
+                    case "жара":
+                    {
+                        Console.WriteLine("heat");
+                        break;
+                    }
+                    case "лёгкий туман":
+                    {
+                        Console.WriteLine("mist");
+                        break;
+                    }
+                    case "туман":
+                    {
+                        Console.WriteLine("fog");
+                        break;
+                    }
+                    case "снег":
+                    {
+                        Console.WriteLine("snow");
+                        break;
+                    }
+                    case "прохладный":
+                    {
+                        Console.WriteLine("cool");
+                        break;
+                    }
+                    case "ливень":
+                    {
+                        Console.WriteLine("shower");
+                        break;
+                    }
+                    default:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Вы ввели несуществующее слово!!!");
+                        break;
+                    }
                 }
-                case "дождь":
-                {
-                    Console.WriteLine("rain");
-                    break;
-                }
-                case "холод":
-                {
-                    Console.WriteLine("cold");
-                    break;
-                }
-                case "ветер":
-                {
-                    Console.WriteLine("wind");
-                    break;
-                }
-                case "жара":
-                {
-                    Console.WriteLine("heat");
-                    break;
-                }
-                case "лёгкий туман":
-                {
-                    Console.WriteLine("mist");
-                    break;
-                }
-                case "туман":
-                {
-                    Console.WriteLine("fog");
-                    break;
-                }
-                case "снег":
-                {
-                    Console.WriteLine("snow");
-                    break;
-                }
-                case "прохладный":
-                {
-                    Console.WriteLine("cool");
-                    break;
-                }
-                case "ливень":
-                {
-                    Console.WriteLine("shower");
-                    break;
-                }
-                default:
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Вы ввели несуществующее слово!!!");
-                    break;
-                }
-            }
+            } while (word != "exit");
             Console.ReadKey();
         }
     }

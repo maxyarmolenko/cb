803a451ba9cd8f336d2c6ff897c7667edcb03df8
Max <maksym_yarmolenko@hotmail.com>
2019-09-10 22:54:59 +0300

    TransformationApp is done

--- Starter/L5/TransformationApp/TransformationApp/Program.cs
+++ Starter/L5/TransformationApp/TransformationApp/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace TransformationApp
+﻿using System;
+
+namespace TransformationApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Используя теорему Де Моргана, преобразовываем исходное выражение A | B, в эквивалентное выражение");
+            bool A = true;
+            bool B = false;
+            if (A | B)
+                Console.WriteLine("!(!A & !B) = {0}", !(!A & !B));
+            Console.ReadKey();
         }
     }
 }

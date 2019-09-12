ff45dd354fb66ac705aa7ea87a6278961ed9ae9e
Max <maksym_yarmolenko@hotmail.com>
2019-09-12 12:26:12 +0300

    ConsoleCalculatorApp is in progress

--- Starter/L4/ConsoleCalculator/ConsoleCalculator/Program.cs
+++ Starter/L4/ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -1,9 +1,57 @@
-﻿namespace ConsoleCalculator
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Enter 1st number");
+            int operand1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int operand2 = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Enter the sign of the arithmetic operation");
+            string sign  = Console.ReadLine();
+            int result =0;
+            int.TryParse(sign, out result);
+            switch (sign)
+            {
+                case "-":
+                {
+                    result = operand1 - operand2;
+                        Console.WriteLine("Arithmetic operation = {0}", result);
+                    break;
+                }
+                case "+":
+                {
+                    result = operand1 + operand2;
+                    Console.WriteLine("arithmetic operation = {0}", result);
+                    break;
+                }
+                case "*":
+                {
+                    result = operand1 * operand2;
+                    Console.WriteLine("arithmetic operation = {0}", result);
+                    break;
+                }
+                case "/":
+                {
+                    if (operand2 != 0)
+                    {
+                        result = operand1 / operand2;
+                        Console.WriteLine("arithmetic operation = {0}", result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Divide by 0 is forbidden");
+                    }
+                    break;
+                }
+
+            }
+                   Console.ReadKey();
+        }          
     }
 }
+ 

caf7fa9c128a0ce71ed026a86fbb1cae206b66c8
Max <maksym_yarmolenko@hotmail.com>
2019-09-14 09:38:53 +0300

    L4/Task3/DefinitionNumbersApp is done

--- Starter/L4/DefinitionNumbersApp/DefinitionNumbersApp/Program.cs
+++ Starter/L4/DefinitionNumbersApp/DefinitionNumbersApp/Program.cs
@@ -4,9 +4,31 @@ namespace DefinitionNumbersApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-           
+         Console.WriteLine("Enter number from 0 till 100");
+         int number = Convert.ToInt32(Console.ReadLine());
+         if (number >= 0 && number < 15)
+         {
+             Console.WriteLine("You entered a number = {0}. It's  numerical range from 0 to 14", number);
+         }
+         if (number >= 15 && number < 36)
+         {
+             Console.WriteLine("You entered a number = {0}. It's a numerical range from 15 to 35", number);
+         }
+         if (number >= 36 && number < 51)
+         {
+             Console.WriteLine("You entered a number = {0}. It's a numerical range from 36 to 50", number);
+         }
+         if (number >= 50 && number < 101)
+         {
+             Console.WriteLine("You entered a number = {0}. It's a numerical range from 50 to 100", number);
+         }
+         else if (number < 0 || number > 100)
+         {
+             Console.WriteLine("You entered a number = {0}. It's not included in the numerical range from 0 to 100", number);
+         }
+         Console.ReadKey();
         }
     }
 }

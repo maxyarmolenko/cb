def0dd27748ea3ed95cb45f13f0099fd28999069
Max <maksym_yarmolenko@hotmail.com>
2019-09-15 22:21:22 +0300

    L6/Task1/RectangleOfStarsApp is done

--- Starter/L6/RectangleOfStarsApp/RectangleOfStarsApp/Program.cs
+++ Starter/L6/RectangleOfStarsApp/RectangleOfStarsApp/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace RectangleOfStarsApp
+﻿using System;
+
+namespace RectangleOfStarsApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Rectangle of stars");
+            int line = 0;
+            while (line < 10)
+            {
+                int star;
+                for (star = 0; star < 50; star++)
+                {
+                    Console.Write("*");
+                }
+                line++;
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }

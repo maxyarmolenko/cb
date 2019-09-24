67496e874f58165d9aae42d140ab7257af68ccc1
Max <maksym_yarmolenko@hotmail.com>
2019-09-24 16:52:18 +0300

    L6/Task4/ProductDeliveryApp is done

--- Starter/L6/ProductDeliveryApp/ProductDeliveryApp/Program.cs
+++ Starter/L6/ProductDeliveryApp/ProductDeliveryApp/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace ProductDeliveryApp
+﻿using System;
+namespace ProductDeliveryApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number of customers to whom the manufacturer have to deliver the product");
+            var counter = Convert.ToInt32(Console.ReadLine());
+           int factorial = 1;
+           do
+            {
+                factorial *= counter--;
+            }
+            while (counter > 0);
+            Console.WriteLine("Maximum number of delivery options, {0}", factorial);
+            Console.ReadKey();
         }
     }
 }

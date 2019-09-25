b2357654e71c4564c1d0dfcbd5b35002966651ea
Max <maksym_yarmolenko@hotmail.com>
2019-09-25 11:12:32 +0300

    L6/Task2/TheSumAndOddNumbersApp is finished

--- Starter/L6/TheSumAndOddNumbersApp/OddNumbersApp/Program.cs
+++ Starter/L6/TheSumAndOddNumbersApp/OddNumbersApp/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace OddNumbersApp
+﻿using System;
+namespace OddNumbersApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number A");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number B");
+            var b = Convert.ToInt32(Console.ReadLine());
+            if (a < b)
+            {
+                do
+                {
+                    b--;
+                    if ((b % 2) != 0)
+                        Console.WriteLine("Odd value located between A and B numbers {0}", b);
+                }
+                while (b > a);
+            }
+            else
+                Console.WriteLine("You entered incorrect number! Number A must be under then number B");
+            Console.ReadKey();
         }
     }
 }
--- Starter/L6/TheSumAndOddNumbersApp/TheSumAndOddNumbersApp/Program.cs
+++ Starter/L6/TheSumAndOddNumbersApp/TheSumAndOddNumbersApp/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace TheSumNumbersApp
+﻿using System;
+namespace TheSumNumbersApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number A");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number B");
+            var b = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            b--;
+            if (a < b)
+            {
+                do
+                {
+                    sum += b;
+                    b--;
+                }
+                while (b > a);
+                Console.WriteLine("The sum of all numbers located between A and B numbers {0}", sum);
+            }
+            else
+                Console.WriteLine("You entered incorrect number! Number A must be under then number B");
+            Console.ReadKey();
         }
     }
 }

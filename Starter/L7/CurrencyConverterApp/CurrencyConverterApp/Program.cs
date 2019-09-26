16133e644e98777c36fe6cb813ecb753a6f28494
Max <maksym_yarmolenko@hotmail.com>
2019-09-26 22:10:31 +0300

    L7/Task3/CurrencyConverterApp is done

--- Starter/L7/CurrencyConverterApp/CurrencyConverterApp/Program.cs
+++ Starter/L7/CurrencyConverterApp/CurrencyConverterApp/Program.cs
@@ -3,8 +3,22 @@ namespace CurrencyConverterApp
 {
     class Program
     {
+        static void CurrencyConverterMethod(ref double quantity, ref double exchangeRates, out double result)
+        {
+            result = quantity * exchangeRates;
+            Console.WriteLine("Amount of money in the chosen currency = {0}", result);
+            Console.ReadKey();
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter amount of money");
+            string operand1 = Console.ReadLine();
+            double quantity = Double.Parse(operand1);
+            Console.WriteLine("Please enter current rate of the selected currency");
+            string operand2 = Console.ReadLine();
+            double exchangeRates = Double.Parse(operand2);
+            double result = 0;
+            CurrencyConverterMethod(ref quantity, ref exchangeRates, out result);
         }
     }
 }

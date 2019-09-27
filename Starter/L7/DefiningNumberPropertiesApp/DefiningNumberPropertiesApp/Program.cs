f1a35b44afebad21400fcd8a0f9e93d4a254f100
Max <maksym_yarmolenko@hotmail.com>
2019-09-27 17:17:28 +0300

    DefiningNumberPropertiesApp is done

--- Starter/L7/DefiningNumberPropertiesApp/DefiningNumberPropertiesApp/Program.cs
+++ Starter/L7/DefiningNumberPropertiesApp/DefiningNumberPropertiesApp/Program.cs
@@ -1,39 +1,68 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
 namespace DefiningNumberPropertiesApp
 {
     class Program
     {
-        static void PosOrNegNumber(ref int a)
+        static void PosOrNegNumber(int number)
         {
-            if (a > 0)
+            if (number > 0)
             {
-                Console.WriteLine("Number is positive, {0}", a);
+                Console.WriteLine("Number is positive, {0}", number);
             }
-            if (a > 0)
+            if (number < 0)
             {
-                Console.WriteLine("Number is negative, {0}", a);
+                Console.WriteLine("Number is negative, {0}", number);
+            }
+            if (number == 0)
+            {
+                Console.WriteLine("Number isn't positive and negative, {0}", number);
+            }
+            Console.ReadKey();
+        }
+        private static void Primenumber(int number)
+        {
+            int checkNumber = 2;
+            int remainder = 0;
+            do
+            {
+                remainder = number % checkNumber;
+                if (remainder != 0)
+                    checkNumber++;    
+            }
+            while (remainder !=0);
+            if (checkNumber == number)
+            {
+                Console.WriteLine("The number is prime");
             }
             else
             {
-                Console.WriteLine("Number isn't positive and negative, {0}", a);
+               Console.WriteLine("The number isn't prime");   
             }
-
-            Console.ReadLine();
+            Console.ReadKey();
         }
-
-        private static void Primenumber(ref int a)
+        static void DivisionWithoutRemainder(int number)
         {
-       
-
-
-
-
-
-
+            if ((number % 2) == 0 && (number % 5) == 0 && (number % 3) == 0 && (number % 6) == 0 && (number % 9) == 0)
+            {
+                Console.WriteLine("The number is divisible by 2, 5, 3, 6, 9, {0}", number);
+            }
+            else
+            {
+                Console.WriteLine("The number isn't divisible by 2, 5, 3, 6, 9, {0}", number);
+            }
+            Console.ReadKey();
         }
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Enter the number to check");
+            string operand1 = Console.ReadLine();
+            int number = Int32.Parse(operand1);
+            PosOrNegNumber(number);
+            Primenumber(number);
+            DivisionWithoutRemainder(number);
+            Console.ReadKey();
         }
     }
 }

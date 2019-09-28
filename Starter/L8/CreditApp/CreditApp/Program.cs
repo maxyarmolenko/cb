310cb491f527ef9139ccfb8c15806209be649517
Max <maksym_yarmolenko@hotmail.com>
2019-09-28 22:40:03 +0300

    L8/Task2/CreditApp is done

--- Starter/L8/CreditApp/CreditApp/Program.cs
+++ Starter/L8/CreditApp/CreditApp/Program.cs
@@ -3,13 +3,33 @@ namespace CreditApp
 {
     class Program
     {
-        static void Loanstatus(int credit)
+        static void Loanstatus(int payment)
         {
+            Console.WriteLine ("Your payment is {0}", payment); 
+            int amountOfDebt = 0;
+            if (payment >= 700)
+            {
+                Console.WriteLine("You haven't any amount of debt", payment);
+            }
+            if (payment < 700)
+            {
+                amountOfDebt = 700 - payment;
+                Console.WriteLine("Your amountOfDebt is {0}", amountOfDebt);
+            }
+            if (payment > 700)
+            {
+                int overpayment = payment - 700;
+                Console.WriteLine("Overpayment is {0}", overpayment);
+            }
 
         }
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Please enter payment amount");
+            int payment;
+            int.TryParse(Console.ReadLine(), out payment);
+            Loanstatus(payment);
+            Console.ReadKey();
         }
     }
 }

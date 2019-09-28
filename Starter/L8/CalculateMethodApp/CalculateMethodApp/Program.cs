b2ced634efaa2d6e2f8cdc5147fdb640f734ae5c
Max <maksym_yarmolenko@hotmail.com>
2019-09-28 13:50:45 +0300

    L8/Task1/CalculateMethodApp is created

---
+++ Starter/L8/CalculateMethodApp/CalculateMethodApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CalculateMethodApp
+{
+    class Program
+    {
+        static int Calculate(int number1, int number2, int number3)
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Calculate();
+        }
+    }
+}

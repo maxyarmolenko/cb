bb85c35c555c0f3069b502bb00dc18df9ce4a291
Max <maksym_yarmolenko@hotmail.com>
2019-09-28 20:02:23 +0300

    L8/Task1/CalculateMethodApp is done

--- Starter/L8/CalculateMethodApp/CalculateMethodApp/Program.cs
+++ Starter/L8/CalculateMethodApp/CalculateMethodApp/Program.cs
@@ -3,13 +3,28 @@ namespace CalculateMethodApp
 {
     class Program
     {
-        static int Calculate(int number1, int number2, int number3)
+        static void Calculate(ref int number1, ref int number2, ref int number3)
         {
-            return 0;
+          number1 = number1 / 5;
+          number2 = number2 / 5;
+          number3 = number3 / 5;
         }
         static void Main(string[] args)
         {
-            Calculate();
+            Console.WriteLine("Please enter 1st integer value");
+            int val1;
+            int.TryParse(Console.ReadLine(), out val1);
+            Console.WriteLine("Please enter 2nd integer value");
+            int val2;
+            int.TryParse(Console.ReadLine(), out val2);
+            Console.WriteLine("Please enter 3rd integer value");
+            int val3;
+            int.TryParse(Console.ReadLine(), out val3);
+            Calculate(ref val1, ref val2, ref val3);
+            Console.WriteLine("Divide by 5 for the 1st value = {0}", val1);
+            Console.WriteLine("Divide by 5 for the 2nd value = {0}", val2);
+            Console.WriteLine("Divide by 5 for the 3rd value = {0}", val3);
+            Console.ReadKey();
         }
     }
 }

6ef7706b381636c30785f0fc7fa18d76d4f34c86
Max <maksym_yarmolenko@hotmail.com>
2019-09-29 09:03:02 +0300

    L8/Task3/DeliveryApp is created

---
+++ Starter/L8/DeliveryApp/DeliveryApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DeliveryApp
+{
+    class Program
+    {
+        static void Factorial(string[] args)
+        {
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}

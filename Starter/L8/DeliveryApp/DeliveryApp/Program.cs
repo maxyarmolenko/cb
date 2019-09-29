cabaecd0163795f7ced1f87f63ce6d42a8eb5dad
Max <maksym_yarmolenko@hotmail.com>
2019-09-29 20:48:40 +0300

    L8/Task3/DeliveryApp is done

--- Starter/L8/DeliveryApp/DeliveryApp/Program.cs
+++ Starter/L8/DeliveryApp/DeliveryApp/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
 namespace DeliveryApp
 {
     class Program
     {
-        static void Factorial(string[] args)
+        static int Factorial (int fact)
         {
+            if (fact == 0)
+              return 1;
+
+            int res = Factorial(fact - 1);
+            return res * fact;
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter quantity of clients");
+            int clientsQuantity;
+            int.TryParse(Console.ReadLine(), out clientsQuantity);
+            int result = Factorial(clientsQuantity);
+            Console.WriteLine("Max quantity of  products delivery for one good is {0}", result );
+            Console.ReadLine();
         }
     }
 }

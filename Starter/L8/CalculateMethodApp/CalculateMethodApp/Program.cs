59a2b85a4d9bfb39e36cdb34c6e6e2d4b30a917f
Max <maksym_yarmolenko@hotmail.com>
2019-09-30 10:50:59 +0300

    L8/Task1/CalculateMethodApp is changed

--- Starter/L8/CalculateMethodApp/CalculateMethodApp/Program.cs
+++ Starter/L8/CalculateMethodApp/CalculateMethodApp/Program.cs
@@ -1,13 +1,40 @@
 ﻿using System;
+using System.IO;
+
 namespace CalculateMethodApp
 {
     class Program
     {
         static void Calculate(int number1, int number2, int number3)
         {
-          number1 = number1 / 5;
-          number2 = number2 / 5;
-          number3 = number3 / 5;
+            if (number1 % 5 == 0)
+            {
+               number1 = number1 / 5;
+               Console.WriteLine("Divide by 5 for the 1st value = {0}", number1);
+            }
+            else
+            {
+                Console.WriteLine("1st value isn't divide by 5 without remainder");
+            }
+            if (number2 % 5 == 0)
+            {
+                number2 = number2 / 5;
+                Console.WriteLine("Divide by 5 for the 2nd value = {0}", number2);
+            }
+            else
+            {
+                Console.WriteLine("2nd value isn't divide by 5 without remainder");
+            }
+            if (number3 % 5 == 0)
+            {
+                number3 = number3 / 5;
+                Console.WriteLine("Divide by 5 for the 3rd value = {0}", number3);
+            }
+            else
+            {
+                Console.WriteLine("3rd value isn't divide by 5 without remainder");
+            }
+            Console.ReadLine();
         }
         static void Main(string[] args)
         {
@@ -21,9 +48,6 @@ namespace CalculateMethodApp
             int val3;
             int.TryParse(Console.ReadLine(), out val3);
             Calculate(val1, val2, val3);
-            Console.WriteLine("Divide by 5 for the 1st value = {0}", val1);
-            Console.WriteLine("Divide by 5 for the 2nd value = {0}", val2);
-            Console.WriteLine("Divide by 5 for the 3rd value = {0}", val3);
             Console.ReadKey();
         }
     }

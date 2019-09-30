3dbbc8cf28d5a32e02b308ad0ae067af2ff486e5
Max <maksym_yarmolenko@hotmail.com>
2019-09-30 22:01:00 +0300

    L9/Task1/ReverseOrderApp is done

--- Starter/L9/ReverseOrderApp/ReverseOrderApp/Program.cs
+++ Starter/L9/ReverseOrderApp/ReverseOrderApp/Program.cs
@@ -5,6 +5,19 @@ namespace ReverseOrderApp
     {
         static void Main(string[] args)
         {
+            byte [] array = new byte[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            Console.WriteLine("Display array elements in order");
+            for (int a = 0; a < array.Length; ++a)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("Display array elements in reverse order");
+
+            for (int b = array.Length - 1; b >= 0; --b)
+            {
+                Console.WriteLine(b);
+            }
+            Console.ReadKey();
         }
     }
 }

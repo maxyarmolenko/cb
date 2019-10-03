f818207a54a51d99f690232cc1ab3b9c40b404c8
Max <maksym_yarmolenko@hotmail.com>
2019-10-03 20:17:05 +0300

    L3/Task3/AverageCalculationApp is done

--- Starter/L3/AverageCalculationApp/AverageCalculationApp/Program.cs
+++ Starter/L3/AverageCalculationApp/AverageCalculationApp/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace AverageCalculationApp
+﻿using System;
+
+namespace AverageCalculationApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Please enter the 1st number");
+            double a;
+            double.TryParse(Console.ReadLine(), out a);
 
+            Console.WriteLine("Please enter the 2nd number");
+            double b;
+            double.TryParse(Console.ReadLine(), out b);
+
+            Console.WriteLine("Please enter the 3rd number");
+            double c;
+            double.TryParse(Console.ReadLine(), out c);
+
+            double average = (a + b + c) / 3;
+
+            Console.Write($"The average value of the entered numbers is {average}");
+            Console.ReadKey();
         }
     }
 }

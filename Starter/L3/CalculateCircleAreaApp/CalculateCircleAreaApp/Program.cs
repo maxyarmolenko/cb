05c57473325d8580bf2053ef18360b450a8dc01e
Max <maksym_yarmolenko@hotmail.com>
2019-10-03 20:37:53 +0300

    L3/Task4/CalculateCircleAreaApp is done

--- Starter/L3/CalculateCircleAreaApp/CalculateCircleAreaApp/Program.cs
+++ Starter/L3/CalculateCircleAreaApp/CalculateCircleAreaApp/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace CalculateCircleAreaApp
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace CalculateCircleAreaApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            const double pi = 3.141592;
+            double r = 1.8;
+
+            double circleArea =  pi * Math.Pow(r, 2);
+
+            Console.WriteLine($"The area of the circle is {circleArea}");
+            Console.ReadKey();
         }
     }
 }

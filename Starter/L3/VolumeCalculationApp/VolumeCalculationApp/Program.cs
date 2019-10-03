735b5046a97ef6e50726f30ac02e41434d8d61fa
Max <maksym_yarmolenko@hotmail.com>
2019-10-03 21:07:23 +0300

    L3/Task5/VolumeCalculationApp is done

--- Starter/L3/VolumeCalculationApp/VolumeCalculationApp/Program.cs
+++ Starter/L3/VolumeCalculationApp/VolumeCalculationApp/Program.cs
@@ -5,6 +5,21 @@ namespace VolumeCalculationApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter cylinder radius");
+            int radius;
+            int.TryParse(Console.ReadLine(), out radius);
+
+            Console.WriteLine("Please enter cylinder height");
+            int height;
+            int.TryParse(Console.ReadLine(), out height);
+
+            const double pi = 3.1415;
+            double v = pi * Math.Pow(radius, 2) * height;
+            Console.WriteLine($"The volume of the cylinder = {v}");
+
+            double s = 2 * pi * Math.Pow(radius, 2) + 2 * pi * Math.Pow(radius, 2);                                   
+            Console.WriteLine($"Cylinder surface area = {s}");
+            Console.ReadKey();
         }
     }
 }

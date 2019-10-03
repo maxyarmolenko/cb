89343613c8d7f3eb95f2f74532ba6a1b6503c43a
Max <maksym_yarmolenko@hotmail.com>
2019-10-03 19:53:04 +0300

    L3/Task2/IntVariablesApp is done

--- Starter/L3/IntVariablesApp/IntVariablesApp/IntVariablesApp/Program.cs
+++ Starter/L3/IntVariablesApp/IntVariablesApp/IntVariablesApp/Program.cs
@@ -7,13 +7,23 @@ namespace IntVariablesApp
         static void Main(string[] args)
         {
             int x = 10, y = 12, z = 3;
-            x += y - x++ * z;
-            Console.WriteLine(x);
-            z = --x – y * 5; }
 
-        Console.WriteLine();
-            Console.WriteLine(z);
+            x += y - (x++ * z);
+            Console.WriteLine("Result of the 1st operation = {0}", x);
+
+            z = --x - y * 5;
+            Console.WriteLine("Result of the 2nd operation = {0}", z);
+
+            y /= x + 5 % z;
+            Console.WriteLine("Result of the 3rd operation = {0}", y);
+
+            z = x++ + y * 5;
+            Console.WriteLine("Result of the 4th operation = {0}", z);
+
+            x = y - x++ * z;
+            Console.WriteLine("Result of the 5th operation = {0}", x);
+
             Console.ReadKey();
         }
     }
-}
+    }

d0b69ec1db5607be0edf4618b3fae417a4704217
Max <maksym_yarmolenko@hotmail.com>
2019-10-06 13:59:27 +0300

    L9/Task4/ArrayLengthApp is in the process

--- Starter/L9/ArrayLengthApp/ArrayLengthApp/Program.cs
+++ Starter/L9/ArrayLengthApp/ArrayLengthApp/Program.cs
@@ -5,6 +5,45 @@ namespace ArrayLengthApp
     {
         static void Main(string[] args)
         {
+            var array = GetArray(10);
+            DisplayArray(array);
+            Console.WriteLine();
+            var inc = IncreaseArrayLength(array);
+            DisplayArray(inc);
+            Console.ReadKey();
+        }
+
+        static int[] GetArray(int a)
+        {
+            var array = new int[a];
+            for (int i = 0; i < a; i++)
+            {
+                array[i] = i;
+            }
+
+            return array;
+        }
+
+        static int[] IncreaseArrayLength(int [] array)
+        {
+            var a = array.Length + 1;
+            var increase = new int[a];
+            for (int i = 0; i < a; i++)
+            {
+                increase[i] = i;
+            }
+
+            return increase;
+        }
+
+        static void DisplayArray(int [] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+              
+            }
+
         }
     }
 }

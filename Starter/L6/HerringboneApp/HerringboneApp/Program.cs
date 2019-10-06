96699ad25055a08b38072b2b7aacda35668b564b
Max <maksym_yarmolenko@hotmail.com>
2019-10-06 23:12:37 +0300

    L6/Task5/HerringboneApp is created

---
+++ Starter/L6/HerringboneApp/HerringboneApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace HerringboneApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static void EquilateralTriangle(int width, int height)
+        {
+           
+        }
+
+        private static void Rectangle(int width, int height)
+        {
+
+        }
+
+    }
+    }
+

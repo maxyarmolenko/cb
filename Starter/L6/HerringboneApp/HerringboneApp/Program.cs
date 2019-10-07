d48449e4d2ab9243d1697362466fddf3ff352da2
Max <maksym_yarmolenko@hotmail.com>
2019-10-07 17:56:12 +0300

    L6/Task5/HerringboneApp is done

--- Starter/L6/HerringboneApp/HerringboneApp/Program.cs
+++ Starter/L6/HerringboneApp/HerringboneApp/Program.cs
@@ -1,23 +1,104 @@
 ﻿using System;
+
 namespace HerringboneApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int height = 7;
+            int width = 5;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            for (int i = 0; i < 7; ++i)
+            {
+                for (int o = 0; o < 10; ++o)
+                {
+                    Console.Write(" ");
+                }
 
-        }
+                for (int j = -15; j < height; ++j)
+                {
+                    if (j > width && j < height)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
 
-        private static void EquilateralTriangle(int width, int height)
-        {
-           
-        }
+                Console.WriteLine();
+                --width;
+                ++height;
+            }
 
-        private static void Rectangle(int width, int height)
-        {
+            {
+                int height1 = 7;
+                int width1 = 5;
+                for (int i = -2; i < 7; ++i)
+                {
+                    for (int o = 0; o < 10; ++o)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = -15; j < height1; ++j)
+                    {
+                        if (j > width1 && j < height1)
+                            Console.Write("*");
+                        else
+                            Console.Write(" ");
+                    }
+
+                    Console.WriteLine();
+                    --width1;
+                    ++height1;
+                }
+
+                {
+                    int height2 = 7;
+                    int width2 = 5;
+                    for (int i = -4; i < 7; ++i)
+                    {
+                        for (int o = 0; o < 10; ++o)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int j = -15; j < height2; ++j)
+                        {
+                            if (j > width2 && j < height2)
+                                Console.Write("*");
+                            else
+                                Console.Write(" ");
+                        }
+
+                        Console.WriteLine();
+                        --width2;
+                        ++height2;
+                    }
+
+                    for (int p = 0; p < 6; ++p)
+                    {
+                        for (int o = 0; o < 30; ++o)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int s = 0; s < 3; ++s)
+                        {
+                            Console.Write("*");
+                        }
+
+                        Console.WriteLine();
+                    }
+
+                    Console.ReadKey();
+                }
+
+            }
 
         }
 
     }
-    }
+
+}
+    
 
